2f3bf2a3436b787ba01414134a71a7a5b68b94ba
Nicolis Yuvienco <nickyuvienco@gmail.com>
2024-10-02 02:02:51 -0700

    remove comments

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -85,43 +85,6 @@ return {
     end,
   },
 
-  -- customize alpha options
-  -- {
-  --   "goolord/alpha-nvim",
-  --   config = function()
-  --     local alpha = require "alpha"
-  --     local dashboard = require "alpha.themes.dashboard"
-  --     local banners = require "banners"
-  --     local function pick_color()
-  --       local colors = { "String", "Identifier", "Keyword", "Number" }
-  --       return colors[math.random(#colors)]
-  --     end
-  --     local function footer()
-  --       local datetime = os.date " %d-%m-%Y   %H:%M:%S"
-  --       local version = vim.version()
-  --       local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
-  --
-  --       return datetime .. "    " .. nvim_version_info
-  --     end
-  --
-  --     -- header config
-  --     dashboard.section.header.val = banners.greymon_crop
-  --     dashboard.section.header.opts.hl = pick_color()
-  --
-  --     --buttons config
-  --     dashboard.section.buttons.val = {
-  --       dashboard.button("<Leader>ff", "  File Explorer"),
-  --     }
-  --
-  --     -- footer config
-  --     -- dashboard.section.footer.val = footer()
-  --     -- dashboard.section.footer.opts.hl = "Constant"
-  --
-  --     alpha.setup(dashboard.opts)
-  --     vim.cmd [[ autocmd FileType alpha setlocal nofoldenable ]]
-  --   end,
-  -- },
-
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = false },
 
@@ -129,7 +92,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -139,7 +102,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -147,18 +110,18 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
