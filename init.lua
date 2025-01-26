eb1dbc8922809a786647c7c8843f09d8a3d5404b
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-01-26 15:51:28 -0800

    format init.lua

--- init.lua
+++ init.lua
@@ -3,14 +3,17 @@
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- validate that lazy is available
 if not pcall(require, "lazy") then
   -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.api.nvim_echo(
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    true, {})
   vim.fn.getchar()
   vim.cmd.quit()
 end
@@ -21,10 +24,10 @@ require "banners"
 
 --   needed for 100% transparency
 require("notify")
-  ---@diagnostic disable-next-line: missing-fields
-  .setup {
-    background_colour = "#000000",
-  }
+---@diagnostic disable-next-line: missing-fields
+    .setup {
+      background_colour = "#000000",
+    }
 
 -- Yank to clipboard WSL2 https://www.reddit.com/r/neovim/comments/vxdjyb/comment/iknh207/
 vim.opt.clipboard = "unnamedplus"
