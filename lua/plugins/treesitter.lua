6f6a5e75f86f3781c9a40f3789e202c9e87efb36
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-01-26 15:51:45 -0800

    disable diagnostic for line

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,7 @@ vim.opt.runtimepath:append "$HOME/.local/share/treesitter"
 
 return {
   ---@type LazySpec
+  ---@diagnostic disable-next-line: missing-fields
   require("nvim-treesitter.configs").setup {
     parser_install_dir = "$HOME/.local/share/treesitter",
     ensure_installed = { "lua", "vim", "html", "css", "jsonc", "typescript" },
