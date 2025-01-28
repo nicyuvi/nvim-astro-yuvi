ce41e2cc48ac92e8b39c87ff2e1baa112d4009d5
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-01-27 21:58:31 -0800

    install default parsers to avoid treesitter errors
    - markdown is still getting a treesitter error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   ---@diagnostic disable-next-line: missing-fields
   require("nvim-treesitter.configs").setup {
     parser_install_dir = "$HOME/.local/share/treesitter",
-    ensure_installed = { "lua", "vim", "html", "css", "jsonc", "typescript", "markdown" },
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "markdown", "html", "css", "jsonc", "typescript" },
     auto_install = true,
     highlight = {
       enable = true,
