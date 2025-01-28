f907536d29194cb21abbbf52b785dc045d2a0569
Nicolis Yuvienco <nickyuvienco@gmail.com>
2025-01-27 21:21:05 -0800

    add markdown to treesitter ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
   ---@diagnostic disable-next-line: missing-fields
   require("nvim-treesitter.configs").setup {
     parser_install_dir = "$HOME/.local/share/treesitter",
-    ensure_installed = { "lua", "vim", "html", "css", "jsonc", "typescript" },
+    ensure_installed = { "lua", "vim", "html", "css", "jsonc", "typescript", "markdown" },
     auto_install = true,
     highlight = {
       enable = true,
